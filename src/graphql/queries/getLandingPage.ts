b171905b023b3255c3096d355487b20b9b5a63b3
João Vitor <jsoareez@gmail.com>
2020-09-14 21:45:45 -0300

    turning sections Hero and AP dynamic

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,12 +1,44 @@
 import { gql } from 'graphql-request';
 
 const GET_LANDING_PAGE = gql`
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
         alternativeText
         url
       }
+    }
+  }
+
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+      title
+      description
+      image {
+        alternativeText
+        url
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
+      ...sectionAboutProject
     }
   }
 `;
